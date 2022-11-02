d517cf010eebc3bae3a023f952e5d38098b776a4
Orel-Viktor <OrelViktor93@gmail.com>
2022-11-02 16:36:30 +0200

    homework19-redy

--- main.js
+++ main.js
@@ -1,46 +1,50 @@
+'use sctrict'
 import "./style.css";
+
 
 const Weather = function () {
   const chooseCity = document.querySelector(".js--choose-city");
   const sendBtn = document.querySelector(".js--send");
+  const cityName = document.querySelector(".js--city-name");
+  const cityTemp = document.querySelector(".js--temp");
+  const cityPressure = document.querySelector(".js--pressure");
+  const cityDescription = document.querySelector(".js--description");
+  const cityHumidity = document.querySelector(".js--humidity");
+  const citySpeed = document.querySelector(".js--speed");
+  const cityDeg = document.querySelector(".js--deg");
+  const cityIcon = document.querySelector(".js--icon");
+  let cityIconvValue = "undefined";
 
-  this.chooseCity =  () => {
-    chooseCity.addEventListener('change', function()  {
-      this.city = chooseCity.value
-      
-    })
-  }
-
- 
+  this.chooseCity = () => {
+    chooseCity.addEventListener("change", () => {
+      this.city = chooseCity.value;
+      this.city === ""
+        ? sendBtn.setAttribute("disabled", "disabled")
+        : sendBtn.removeAttribute("disabled");
+    });
+  };
 
   this.sendInfo = function () {
-    this.chooseCity()
-  
-    sendBtn.addEventListener('click', () => {
-      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`
-      ).then((res) => res.json()).then((data) => console.log(data));
-    })
-
-  }
-
+    this.chooseCity();
+    sendBtn.addEventListener("click", () => {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&APPID=5d066958a60d315387d9492393935c19`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          cityName.innerHTML = data.name;
+          cityTemp.innerHTML = data.main.temp + "℃";
+          cityPressure.innerHTML = data.main.pressure + " " + "hPa";
+          cityDescription.innerHTML = data.weather.map((elem) => elem.description);
+          cityHumidity.innerHTML = data.main.humidity + "%";
+          citySpeed.innerHTML = data.wind.speed + " " + "km/h SSE";
+          cityDeg.innerHTML = data.wind.deg;
+          cityIconvValue = data.weather.map((elem) => elem.icon);
+          cityIcon.src = `http://openweathermap.org/img/w/${cityIconvValue}.png `;
+        });
+    });
+  };
 };
 
 const weather = new Weather();
-weather.sendInfo()
-
-
-// sendBtn.addEventListener("click", function () {
-//   fetch(
-//     `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`
-//   )
-//     .then((res) => res.json())
-//     .then((data) => console.log(data));
-
-
-// });
-
-
-// chooseCity.addEventListener("change", function () {
-//   let city = chooseCity.value;
-//   console.log(city)
-// });
+weather.sendInfo();
