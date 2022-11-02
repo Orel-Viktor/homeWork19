fddd4703b1fee5d78cf30166b548af400cd23cef
Orel-Viktor <orelviktor93@gamil.com>
2022-11-02 21:13:00 +0300

    rework-homework19

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-'use sctrict'
+"use sctrict";
 import "./style.css";
 
-
 const Weather = function ({
-  selectCity, 
-  btn, 
-  nameCity, 
-  tempCity, 
-  pressureCity, 
-  descriptionCity, 
-  humidityCity, 
-  speedCity, 
-  degCity, 
+  selectCity,
+  btn,
+  nameCity,
+  tempCity,
+  pressureCity,
+  descriptionCity,
+  humidityCity,
+  speedCity,
+  degCity,
   iconCity,
-})
-   {
+}) {
   const chooseCity = document.querySelector(selectCity);
   const sendBtn = document.querySelector(btn);
   const cityName = document.querySelector(nameCity);
@@ -23,17 +21,29 @@ const Weather = function ({
   const cityDescription = document.querySelector(descriptionCity);
   const cityHumidity = document.querySelector(humidityCity);
   const citySpeed = document.querySelector(speedCity);
-  const cityDeg = document.querySelector(degCity, );
+  const cityDeg = document.querySelector(degCity);
   const cityIcon = document.querySelector(iconCity);
-  let cityIconvValue = "undefined";
 
   this.chooseCity = () => {
     chooseCity.addEventListener("change", () => {
       this.city = chooseCity.value;
-      this.city === ""
-        ? sendBtn.setAttribute("disabled", "disabled")
-        : sendBtn.removeAttribute("disabled");
+      this.setText();
+      if (this.city === "") {
+        sendBtn.setAttribute("disabled", "disabled");
+      } else {
+        sendBtn.removeAttribute("disabled");
+      }
     });
+  };
+  this.setText = (values = {}) => {
+    cityName.innerHTML = values.name || "";
+    cityTemp.innerHTML = values.temp || "";
+    cityPressure.innerHTML = values.pressure || "";
+    cityDescription.innerHTML = values.description || "";
+    cityHumidity.innerHTML = values.humidity || "";
+    citySpeed.innerHTML = values.speed || "";
+    cityDeg.innerHTML = values.deg || "";
+    cityIcon.src = values.src || "";
   };
 
   this.sendInfo = function () {
@@ -44,48 +54,47 @@ const Weather = function ({
       )
         .then((res) => res.json())
         .then((data) => {
-          cityName.innerHTML = data.name;
-          cityTemp.innerHTML = data.main.temp + "℃";
-          cityPressure.innerHTML = data.main.pressure + " " + "hPa";
-          cityDescription.innerHTML = data.weather.map((elem) => elem.description);
-          cityHumidity.innerHTML = data.main.humidity + "%";
-          citySpeed.innerHTML = data.wind.speed + " " + "km/h SSE";
-          cityDeg.innerHTML = data.wind.deg;
-          cityIconvValue = data.weather.map((elem) => elem.icon);
-          cityIcon.src = `http://openweathermap.org/img/w/${cityIconvValue}.png `;
+          this.setText({
+            name: data.name,
+            temp: data.main.temp + "℃",
+            pressuredata: data.main.pressure + " " + "hPa",
+            description: data.weather.map((elem) => elem.description),
+            humidity: data.main.humidity + "%",
+            speed: data.wind.speed + " " + "km/h SSE",
+            deg: data.wind.deg,
+            src: `http://openweathermap.org/img/w/${data.weather.map(
+              (elem) => elem.icon
+            )}.png `,
+          });
         });
     });
   };
 };
 
-const weather = new Weather(
-   {
-    selectCity: ".js--choose-city", 
-    btn:".js--send", 
-    nameCity:".js--city-name", 
-    tempCity:".js--temp", 
-    pressureCity:".js--pressure", 
-    descriptionCity:".js--description", 
-    humidityCity:".js--humidity", 
-    speedCity:".js--speed", 
-    degCity:".js--deg", 
-    iconCity:".js--icon",
- }
-);
+const weather = new Weather({
+  selectCity: ".js--choose-city",
+  btn: ".js--send",
+  nameCity: ".js--city-name",
+  tempCity: ".js--temp",
+  pressureCity: ".js--pressure",
+  descriptionCity: ".js--description",
+  humidityCity: ".js--humidity",
+  speedCity: ".js--speed",
+  degCity: ".js--deg",
+  iconCity: ".js--icon",
+});
 weather.sendInfo();
 
-const weather1 = new Weather(
-  {
-    selectCity: ".js--choose-city1", 
-    btn:".js--send1", 
-    nameCity:".js--city-name1", 
-    tempCity:".js--temp1", 
-    pressureCity:".js--pressure1", 
-    descriptionCity:".js--description1", 
-    humidityCity:".js--humidity1", 
-    speedCity:".js--speed1", 
-    degCity:".js--deg1", 
-    iconCity:".js--icon1",
- }
-)
-weather1.sendInfo()
+const weather1 = new Weather({
+  selectCity: ".js--choose-city1",
+  btn: ".js--send1",
+  nameCity: ".js--city-name1",
+  tempCity: ".js--temp1",
+  pressureCity: ".js--pressure1",
+  descriptionCity: ".js--description1",
+  humidityCity: ".js--humidity1",
+  speedCity: ".js--speed1",
+  degCity: ".js--deg1",
+  iconCity: ".js--icon1",
+});
+weather1.sendInfo();
