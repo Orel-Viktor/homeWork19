8dae6ec65c0cdeeff80c1ceac0a0179559642ad4
Orel-Viktor <OrelViktor93@gmail.com>
2022-11-02 13:15:35 +0200

    Jenya-Pamagi

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const Weather = function () {
+  const chooseCity = document.querySelector(".js--choose-city");
+  const sendBtn = document.querySelector(".js--send");
+
+  this.chooseCity =  () => {
+    chooseCity.addEventListener('change', function()  {
+      this.city = chooseCity.value
+      
+    })
+  }
+
+ 
+
+  this.sendInfo = function () {
+    this.chooseCity()
+  
+    sendBtn.addEventListener('click', () => {
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`
+      ).then((res) => res.json()).then((data) => console.log(data));
+    })
+
+  }
+
+};
+
+const weather = new Weather();
+weather.sendInfo()
+
+
+// sendBtn.addEventListener("click", function () {
+//   fetch(
+//     `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`
+//   )
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+
+
+// });
+
+
+// chooseCity.addEventListener("change", function () {
+//   let city = chooseCity.value;
+//   console.log(city)
+// });
